d981ce7f3c699abe88af1feca24a9991c57746a9
osadake212 <osadake.212.dev@gmail.com>
2014-11-19 12:25:34 +0900

    ユーザーモデルの作成 * 仮データを提供するspa.fake.jsを追加 * モデルを管理するspa.model.jsを追加 * ユーザーモデルを作成し、サインイン、サインアウトを実装

--- public/js/spa.js
+++ public/js/spa.js
@@ -4,7 +4,9 @@
  */
 /* global $, spa:true */
 var spa = (function () {
+  'use strict';
   var initModule = function ($container) {
+    spa.model.initModule();
     spa.shell.initModule($container);
   };
 
