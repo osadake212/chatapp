f3df74ac9b3b3280097973a367275642a731e8d5
osadake212 <osadake.212.dev@gmail.com>
2014-11-25 12:25:59 +0900

    チャットモデルの新規作成 * チャットに参加するメソッドの追加 * モデルに合わせてfake.jsの修正

--- public/js/spa.model.js
+++ public/js/spa.model.js
@@ -12,12 +12,13 @@ spa.model = (function () {
       cid_serial: 0,
       people_cid_map: {},
       people_db: TAFFY(),
-      user: null
+      user: null,
+      is_connected: false
     },
 
     isFakeDate = true,
     personProto, makeCid, clearPeopleDb, completeLogin,
-    makePerson, removePerson, people, initModule;
+    makePerson, removePerson, people, chat, initModule;
 
     // personオブジェクトのプロトタイプ
     personProto = {
@@ -96,6 +97,7 @@ spa.model = (function () {
       return true;
     };
 
+    // ----- people model -----
     people = ( function () {
       var get_by_cid, get_db, get_user, login, logout;
 
@@ -145,10 +147,77 @@ spa.model = (function () {
       };
     }());
 
+    // ----- chat model -----
+    chat = (function () {
+      var
+        _publish_listchange,
+        _update_list, _leave_chat, join_chat;
+
+        // ユーザーリストを更新する
+        _update_list = function (arg_list) {
+          var i, person_map, make_person_map,
+            people_list = arg_list[0];
+
+          clearPeopleDb();
+
+          for (i = 0; i < people_list.length; ++i) {
+            person_map = people_list[i];
+            if (!person_map.name) {
+              continue;
+            }
+
+            // ユーザーを特定したらcss_mapを更新する
+            if (stateMap.user && stateMap.user.id === person_map._id) {
+              stateMap.user.css_map = person_map.css_map;
+              continue;
+            }
+
+            make_person_map = {
+              cid: person_map._id,
+              css_map: person_map.css_map,
+              id: person_map._id,
+              name: person_map.name
+            };
+
+            makePerson(make_person_map);
+          }
+
+          stateMap.people_db.sort('name');
+        };
+
+        _publish_listchange = function (arg_list) {
+          _update_list(arg_list);
+          $.gevent.publish('spa-listchange', [arg_list]);
+        };
+
+        join_chat = function () {
+          var sio;
+
+          if (stateMap.is_connected) {
+            return false;
+          }
+
+          if (stateMap.user.get_is_anon()) {
+            console.warn('User must be defined before joining chat');
+            return false;
+          }
+
+          sio = isFakeDate ? spa.fake.mockSio : spa.data.getSio();
+          sio.on('listchange', _publish_listchange);
+          stateMap.is_connected = true;
+          return true;
+        };
+
+        return {
+          _leave: _leave_chat,
+          join: join_chat
+        };
+    }());
+
+
     initModule = function () {
       var i, people_list, person_map;
 
-      clearPeopleDb();
       // 匿名ユーザーを初期化する
       stateMap.anon_user = makePerson({
         cid: configMap.anon_id,
@@ -156,24 +225,11 @@ spa.model = (function () {
         name: 'anonymous'
       });
       stateMap.user = stateMap.anon_user;
-
-      if (isFakeDate) {
-        people_list = spa.fake.getPeopleList();
-        for (i = 0; i < people_list.length; ++i) {
-          person_map = people_list[i];
-          makePerson({
-            cid: person_map._id,
-            css_map: person_map.css_map,
-            id: person_map._id,
-            name: person_map.name
-          });
-        }
-      }
     };
-
 
   return {
     initModule: initModule,
-    people: people
+    people: people,
+    chat: chat
   };
 }());
