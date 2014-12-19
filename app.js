c0ff6a85273c372230713ca1be47afb70aaef9df
osadake212 <osadake.212.dev@gmail.com>
2014-12-19 12:23:13 +0900

    DBに登録するJSONのスキーマの検証を追加 * JSONをCRUDできるようにroutes.jsを調整 * JSVの追加 * JSONスキーマの作成

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var
 app.configure( function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '\\public'));
   app.use(app.router);
 });
 
--- routes.js
+++ routes.js
@@ -6,15 +6,37 @@
 
 'use strict';
 var
-  configRoutes,
+  loadSchema, checkSchema, configRoutes,
   mongodb = require('mongodb'),
-  mongoServer = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT),
-  dbHandle = new mongodb.Db('spa', mongoServer, {safe: true});
+  fsHandle = require('fs'),
+  JSV = require('JSV').JSV,
 
-  dbHandle.open(function () {
-    console.log('** Connected to MongoDB **');
+  mongoServer = new mongodb.Server(
+    'localhost',
+    mongodb.Connection.DEFAULT_PORT),
+  dbHandle = new mongodb.Db('spa', mongoServer, { safe: true }),
+  validator = JSV.createEnvironment(),
+
+  makeMongoId = mongodb.ObjectID,
+  objTypeMap = { 'user': {} };
+
+// ----- ユーティリティメソッド -----
+
+loadSchema = function (schema_name, schema_path) {
+  fsHandle.readFile(schema_path, 'utf8', function (err, data) {
+    objTypeMap[schema_name] = JSON.parse(data);
   });
+};
 
+checkSchema = function (obj_type, obj_map, callback) {
+  var
+    schema_map = objTypeMap[obj_type],
+    report_map = validator.validate(obj_map, schema_map);
+
+  callback(report_map.errors);
+};
+
+// ----- パブリックメソッド -----
 configRoutes = function (app, server) {
 
   app.get('/', function (request, response) {
@@ -23,35 +45,125 @@ configRoutes = function (app, server) {
 
   app.all('/:obj_type/*?', function (request, response, next) {
     response.contentType('json');
-    next();
+
+    if (objTypeMap[request.params.obj_type]) {
+      next();
+    } else {
+      response.send({ error_msg: request.params.obj_type + ' is not a valid object type' });
+    }
   });
 
   app.get('/:obj_type/list', function (request, response) {
-    response.send({ title: request.params.obj_type + ' list' });
+
+    dbHandle.collection(
+      request.params.obj_type,
+      function (outer_error, collection) {
+        collection.find().toArray(
+          function (inner_error, map_list) {
+            response.send(map_list);
+          }
+        );
+      }
+    );
   });
 
   app.post('/:obj_type/create', function (request, response) {
-    response.send({ title: request.params.obj_type + ' created' });
+    dbHandle.collection(
+      request.params.obj_type,
+      function (outer_error, collection) {
+        var
+          options_map = {safe: true},
+          obj_map = request.body;
+        collection.insert(
+          obj_map,
+          options_map,
+          function (inner_error, result_map) {
+            response.send(result_map);
+          }
+        );
+      }
+    );
   });
 
   app.get('/:obj_type/read/:id([0-9]+)', function (request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-    });
+    var find_map = {
+      _id: makeMongoId(request.params.id)
+    };
+    dbHandle.collection(
+      request.params.obj_type,
+      function (outer_error, collection) {
+        collection.findOne(
+          find_map,
+          function (inner_error, result_map) {
+            response.send(result_map);
+          }
+        );
+      }
+    );
   });
 
   app.post('/:obj_type/update/:id([0-9]+)', function (request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
-    });
+    var
+      find_map = { _id: makeMongoId(request.params.id) },
+      obj_map = request.body;
+
+    dbHandle.collection(
+      request.params.obj_type,
+      function (outer_error, collection) {
+        var
+          sort_order = [],
+          options_map = {
+            'new': true, upsert: false, safe: true
+          };
+
+        collection.findAndModify(
+          find_map,
+          sort_order,
+          obj_map,
+          options_map,
+          function (inner_error, updated_map) {
+            response.send(updated_map);
+          }
+        );
+      }
+    );
   });
 
   app.get('/:obj_type/delete/:id([0-9]+)', function (request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-    });
+    var find_map = { _id: makeMongoId(request.params.id) };
+
+    dbHandle.collection(
+      request.params.obj_type,
+      function (outer_error, collection) {
+        var options_map = { safe: true, single: true };
+
+        collection.remove(
+          find_map,
+          options_map,
+          function (inner_error, delete_count) {
+            response.send({ delete_count: delete_count });
+          }
+        );
+      }
+    );
   });
 
 };
 
 module.exports = { configRoutes: configRoutes };
+
+// ----- モジュールの初期化開始 -----
+dbHandle.open(function () {
+  console.log('** Connected to MongoDB **');
+});
+
+// スキーマをメモリにロードする
+(function () {
+  var schema_name, schema_path;
+  for (schema_name in objTypeMap) {
+    if (objTypeMap.hasOwnProperty(schema_name)) {
+      schema_path = __dirname + '\\' + schema_name + '.json';
+      loadSchema(schema_name, schema_path);
+    }
+  }
+}());
